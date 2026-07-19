fac904113aaabf3b27ee609dce05fc0d9a631266
Tubagus Iqbal Husaeni <ikbalgsr46@gmail.com>
2026-07-20 06:04:58 +0700

    Significantly boost the volume of the UI click sound to match loading animation volume

---
+++ boost_click.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy.io import wavfile
+
+def soft_boost(filename, gain):
+    rate, data = wavfile.read(filename)
+    x = data.astype(np.float32) / 32768.0
+    y = np.tanh(x * gain)
+    data_int = np.int16(y * 32767.0)
+    wavfile.write(filename, rate, data_int)
+
+soft_boost('public/assets/audio/steam_click.wav', 4.0)
